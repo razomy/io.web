f4fcdc038314f2bc57bee8626fbe25f811ad5e1b
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2024-07-04 10:50:22 -0400

    replace symlink with files for Docker
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
-gcloud run deploy function --source . --region us-central1 --port=8000  --set-env-vars "DEPLOYMENT=PRODUCTION"
+_CURRENT_DIRECTORY=$(dirname $(realpath "$0"))
+
+
+# convert link to files
+rm $_CURRENT_DIRECTORY/razomy/python
+cp -a $_CURRENT_DIRECTORY/../python/razomy/python $_CURRENT_DIRECTORY/razomy/python
+
+gcloud run deploy function --source . --region us-central1 --port=8000
+
+# convert files to link
+rm -rf $_CURRENT_DIRECTORY/razomy/python
+ln -sf $_CURRENT_DIRECTORY/../python/razomy/python $_CURRENT_DIRECTORY/razomy/python
